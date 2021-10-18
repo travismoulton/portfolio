3ac89c29ab34daadad92ae9d9a0553bac7584f12
Travis <travis.moulton002@gmail.com>
2021-10-18 11:50:28 -0400

    finished contact form

--- components/Contact/ContactForm.jsx
+++ components/Contact/ContactForm.jsx
@@ -5,35 +5,50 @@ import classes from '../../styles/Contact.module.css';
 export default function ContactForm() {
   const [formState, handleSubmit] = useForm('xoqyzewv');
 
+  if (formState.succeeded)
+    return (
+      <p>
+        Thank you for reaching out! I will review your message and get back to
+        you shortly.
+      </p>
+    );
+
   return (
-    <form className={classes.ContactForm} onSubmit={handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input type="email" id="email" name="email" />
-      <ValidationError prefix="Email" field="email" errors={formState.errors} />
+    <>
+      <h3 className={classes.ContactFormHeader}>Contact Form</h3>
+      <form className={classes.ContactForm} onSubmit={handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input type="email" id="email" name="email" />
+        <ValidationError
+          prefix="Email"
+          field="email"
+          errors={formState.errors}
+        />
 
-      <label htmlFor="name">Your Name</label>
-      <input type="text" id="name" name="name" />
-      <ValidationError prefix="Name" field="name" errors={formState.errors} />
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" name="name" />
+        <ValidationError prefix="Name" field="name" errors={formState.errors} />
 
-      <label htmlFor="subject">Subject</label>
-      <input type="text" id="subject" name="subject" />
-      <ValidationError
-        prefix="Subject"
-        field="subject"
-        errors={formState.errors}
-      />
+        <label htmlFor="subject">Subject</label>
+        <input type="text" id="subject" name="subject" />
+        <ValidationError
+          prefix="Subject"
+          field="subject"
+          errors={formState.errors}
+        />
 
-      <label htmlFor="message">Your Message</label>
-      <textarea name="message" id="message" cols="30" rows="10"></textarea>
-      <ValidationError
-        prefix="Message"
-        field="message"
-        errors={formState.errors}
-      />
+        <label htmlFor="message">Your Message</label>
+        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={formState.errors}
+        />
 
-      <button type="submit" disabled={formState.submitting}>
-        Submit
-      </button>
-    </form>
+        <button type="submit" disabled={formState.submitting}>
+          Submit
+        </button>
+      </form>
+    </>
   );
 }
