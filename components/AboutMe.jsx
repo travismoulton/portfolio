6ed44b12236518a1398ad9a8a09d4adf911596ac
Travis <travis.moulton002@gmail.com>
2021-10-19 09:30:19 -0400

    more minor styling tweaks

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -9,13 +9,13 @@ export default function AboutMe() {
       <div className={classes.TextContainer}>
         <p className={classes.Text}>
           I'm a self taught front-end web developer working on learning the
-          backend and being able to contribute on the full stack. I love solving
-          problems with Javascirpt and building beautiful front ends. I am
-          excited at the prospect of working along side other excellent
-          developers and taking my skills to the next level. My 10 years of
-          exerpience in retail management have given me the opprotunity to
-          develop exceptional communication, interpersonal, and orginizational
-          skills, and make me an excellent addition to your team.
+          backend and being able to contribute to the full stack. I love solving
+          problems with Javascirpt and exploring technology. I am excited at the
+          prospect of working along side other excellent developers and taking
+          my skills to the next level. My 10 years of exerpience in retail
+          management have given me the opprotunity to develop exceptional
+          communication, interpersonal, and orginizational skills, and make me
+          an excellent addition to your team.
         </p>
       </div>
     </section>
