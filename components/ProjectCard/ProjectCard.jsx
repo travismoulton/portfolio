35f40d216b08bb81f3c6652f5ec5177679d12ecf
Travis <travis.moulton002@gmail.com>
2022-03-08 09:33:28 -0500

    adjusted responsive styling in ProjectCard with 3 links

--- components/ProjectCard/ProjectCard.jsx
+++ components/ProjectCard/ProjectCard.jsx
@@ -16,16 +16,26 @@ export default function ProjectCard({
   backEndRepo,
 }) {
   return (
-    <div className={classes.ProjectCard}>
+    <div
+      className={`${classes.ProjectCard} ${
+        title === 'Threaded Chat' && classes.ProjectCard__threadedChat
+      }`}
+    >
       <div className={classes.CardLeft}>
         <ProjectTitle title={title} />
         <ProjectDescription desc={desc} />
-        <div className={classes.ProjectLinks}>
+        <div
+          className={`${classes.ProjectLinks} ${
+            frontEndRepo && classes.ThreeLinks
+          }`}
+        >
           <ProjectLink link={siteLink} linkTxt={siteLinkTxt} />
           {repoLink && <ProjectLink link={repoLink} linkTxt={repoLinkTxt} />}
+
           {frontEndRepo && (
             <ProjectLink link={frontEndRepo} linkTxt="Front End Repo" />
           )}
+
           {backEndRepo && (
             <ProjectLink link={backEndRepo} linkTxt="Back End Repo" />
           )}
