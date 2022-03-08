e436095715b16236572a5817fd499c08e504b33f
Travis <travis.moulton002@gmail.com>
2022-03-08 09:15:17 -0500

    added threadedChat project

--- components/MyWork.jsx
+++ components/MyWork.jsx
@@ -7,6 +7,8 @@ const mtgDesc =
 const workoutDesc =
   "An app that allows the user to search through a database of exercises, track their favorites for easy access, and assemble workouts made up of those exercises. The user can also create a routine and record workouts. The app will suggest the days workout based on the settings in the user's routine. More information can be found the in the README on the Github Repo";
 
+const threadedChatDesc =
+  "A simple reddit clone built as a full stack MERN app. This app features an authentication system built with JWT utilizing cookies. This app features a threaded comment structure, gives users the ability to create groups, post threads, and comment on other user's comments and threads More information can be found the in the README on the Github Repo";
 const MTGDetails = {
   img: 'MTGImage',
   title: 'MTG Power Search',
@@ -26,12 +28,23 @@ const workoutPlannerDetails = {
   repoLink: 'https://github.com/travismoulton/workout-rebuild',
   repoLinkTxt: 'Github Repo',
 };
+
+const threadedChatDetails = {
+  img: 'thredditImg',
+  title: 'Threaded Chat',
+  desc: threadedChatDesc,
+  siteLink: 'https://www.threddit.win',
+  siteLinkTxt: 'Live Site',
+  repoLink: 'https://github.com/travismoulton/social-media-frontend',
+  repoLinkTxt: 'Github Repo',
+};
 export default function MyWork() {
   return (
     <section className={classes.MyWork} id="MyWork">
       <h2 className="Header">My work</h2>
       <ProjectCard {...MTGDetails} />
       <ProjectCard {...workoutPlannerDetails} />
+      <ProjectCard {...threadedChatDetails} />
     </section>
   );
 }
--- components/ProjectCard/ProjectImg.jsx
+++ components/ProjectCard/ProjectImg.jsx
@@ -2,9 +2,10 @@ import Image from 'next/image';
 
 import MTGImage from '../../public/MTGPowerSearchImg.png';
 import WorkoutImg from '../../public/WorkoutImg2.png';
+import thredditImg from '../../public/threddit.png';
 import classes from '../../styles/ProjectCard.module.css';
 
-const images = { MTGImage, WorkoutImg };
+const images = { MTGImage, WorkoutImg, thredditImg };
 
 export default function ProjectImg({ img }) {
   return (
