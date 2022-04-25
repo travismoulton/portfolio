979865c5d64c7c366ebb05b9b03cf8c6216a7f2e
Travis <travis.moulton002@gmail.com>
2022-04-25 08:48:21 -0400

    minified images

--- components/AboutMePic.jsx
+++ components/AboutMePic.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-import img from '../public/me2.jpg';
+import img from '../public/me2-min.jpg';
 import classes from '../styles/AboutMe.module.css';
 
 export default function AboutMePic({ refProp }) {
@@ -11,7 +11,8 @@ export default function AboutMePic({ refProp }) {
         src={img}
         alt="Picture of myself"
         layout="fill"
-        priority
+        loading="eager"
+        priority={true}
       />
     </div>
   );
--- components/ProjectCard/ProjectImg.jsx
+++ components/ProjectCard/ProjectImg.jsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 
-import MTGImage from '../../public/MTGPowerSearchImg.png';
-import WorkoutImg from '../../public/WorkoutImg2.png';
-import thredditImg from '../../public/threddit.png';
+import MTGImage from '../../public/MTGPowerSearchImg-min.png';
+import WorkoutImg from '../../public/WorkoutImg2-min.png';
+import thredditImg from '../../public/threddit-min.png';
 import classes from '../../styles/ProjectCard.module.css';
 
 const images = { MTGImage, WorkoutImg, thredditImg };
